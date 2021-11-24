7d5fb2fb675700fc6fc7f738ec7efabbbe1281eb
Joe Sembler <joeysembler@gmail.com>
2021-11-24 12:32:17 -0500

    commit tree 999fdb6efd7d052035980cd6a8f8b0bda7a97622

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+function findMatching(drivers, arg){
+    const newArray = drivers.filter(
+        function(string){
+            if(string.toLowerCase() == arg.toLowerCase()){
+                return string.toLowerCase;
+            }
+        }
+    );
+    return newArray;
+}
+
+function fuzzyMatch(drivers, arg){
+    const newArray = drivers.filter(
+        function(string){
+            if(string.substring(0,2) === arg.substring(0,2)){
+                return string;
+            }
+        }
+    )
+    return newArray;
+}
+
+function matchName(driver, arg){
+    const newArray = driver.filter(
+        function(driver){
+            if(driver.name === arg){
+                return driver;
+            }
+        }
+    )
+    return newArray;
+}
